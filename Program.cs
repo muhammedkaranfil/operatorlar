486e7d49f7478f94299dbcd3fb6795e9a6d849bc
muhammed abdülhalim kurunful <muhammedkaranfil6991@gmail.com>
2022-02-13 00:51:11 +0300

    int.

--- Program.cs
+++ Program.cs
@@ -34,9 +34,51 @@ using System
               if(isSuccess ! isComplated )
         Console.WriteLine("Fine!");
          Console.WriteLine("*****ilişkisel operatörler*****");
+         //ilişkisel operatörler
+         //<,> ,<=, ==,  !=
+         int a = 3;
+         int b = 4;
+         bool sonuc = a<b;
+           Console.WriteLine(sonuc);
+           sonuc = a>b;
+           Console.WriteLine(sonuc);
+           sonuc = a>=b;
+           Console.WriteLine(sonuc);
+           sonuc =a<=b;
+           Console.WriteLine(sonuc);
+           sonuc = a==b;
+           Console.WriteLine(sonuc);
+           sonuc = a!=b;
+           Console.WriteLine(sonuc);
+           Console.WriteLine("*****aritmetik operatörler*****");
+           // , /, *,  +,-
+           int sayi1 = 10;
+           int sayi2 = 5;
+           int sonuc  = sayi1/sayi2;
+           Console.WriteLine(sonuc);
+           sonuc1 = sayi1* sayi2;
+           Console.WriteLine(sonuc);
+            sonuc1 = sayi1+ sayi2;
+           Console.WriteLine(sonuc);
+            sonuc1 = sayi1 ++;
+           Console.WriteLine(sonuc);
+           // % mod alır
+          int  sonuc2 = 20%3;
+           Console.WriteLine(sonuc2);
+        
+        
+ 
+
+
+
+
+
+
+
 
 
         }
+
     }
 
    
