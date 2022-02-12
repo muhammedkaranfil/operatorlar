4d18a72389ed1a8a941be1ffc400a64e6e7103b8
muhammed abdülhalim kurunful <muhammedkaranfil6991@gmail.com>
2022-02-13 00:27:42 +0300

    int

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace operatorler // Note: actual namespace depends on the project name.
+using System
+
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] args)
+        {
+            //ATAMA VE İŞLEMLİ ATAMA
+
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+         
+            Console.WriteLine("y");
+            y += 2;
+            Console.WriteLine("y");
+            y/=1;
+            Console.WriteLine("y");
+            x  *=2;
+            Console.WriteLine("y");
+        Console.WriteLine("*****Mantıksal operatörler*****");
+        // MANTISAL OPERATÖRLER
+        // ||,&&,!
+        bool isSuccess = true;
+        bool isComplated = false;
+        if(isSuccess && isComplated )
+          Console.WriteLine("Perfect!");
+        if(isSuccess ||isComplated )
+             Console.WriteLine("Great!");
+              if(isSuccess ! isComplated )
+        Console.WriteLine("Fine!");
+         Console.WriteLine("*****ilişkisel operatörler*****");
+
+
+        }
+    }
+
+   
+    {
+    }
+}
+
